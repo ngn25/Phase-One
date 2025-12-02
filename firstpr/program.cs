d9e38d946f42dd766b72362bbbf5f9a27aa02d71
ngn25 <negin.msv80@gmail.com>
2025-12-02 10:53:03 +0330

    update program.cs

--- firstpr/program.cs
+++ firstpr/program.cs
@@ -38,18 +38,71 @@ class Program
                     DateOfBirth = parts[4],
                     Email = parts[5],
                     PhoneNumber = parts[6]
-                    
+
                 };
-            
+
 
                 studentService.Add(student);
                 Console.WriteLine("student added.");
 
 
+
             }
+
+            if (target.Equals("student", StringComparison.OrdinalIgnoreCase) &&
+            action.Equals("GetAll", StringComparison.OrdinalIgnoreCase))
+            {
+                List<Student> allStudents = studentService.GetAll();
+                if (allStudents.count == 0)
+                {
+                    Console.WriteLine("No students not found!");
+                    continue;
+
+                }
+                foreach (Student student in allStudents)
+                {
+                    foreach (Student student in allStudents)
+                    {
+                        Console.WriteLine($"{student.Id} - {student.Name} - {student.DateOfBirth} - {student.Email} - {student.PhoneNumber}");
+                    }
+                }
+            }
+
+
+            if (target.Equals("student", StringComparison.OrdinalIgnoreCase) &&
+                action.Equals("Remove", StringComparison.OrdinalIgnoreCase))
+            {
+                if (parts.Length < 3)
+                {
+                    Console.WriteLine("Not enough parametrs for student remove!");
+                    continue;
+                }
+                string id = parts[2];
+                studentService.Remove(id);
+                continue;
+
+            }
+            if (target.Equals("student", StringComparison.OrdinalIgnoreCase) &&
+                action.Equals("Update", StringComparison.OrdinalIgnoreCase))
+            {
+                if (parts.Length < 7)
+                {
+                    Console.WriteLine("Not enough parametrs for student Update!");
+                    continue;
+                }
+                Student updatedStudent = new Student
+                {
+                    Id = parts[2],
+                    Name = parts[3],
+                    DateOfBirth = parts[4],
+                    Email = parts[5],
+                    PhoneNumber = parts[6]
+                };
+                studentService.Update(updatedStudent);
+                continue;
+            }
+
 
         }
     }
-
-
 }
